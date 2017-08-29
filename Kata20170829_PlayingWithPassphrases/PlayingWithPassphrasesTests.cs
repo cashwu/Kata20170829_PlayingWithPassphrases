da3d2cbebdaff2881d1b58c25738d4b3ba17ddcd
cash <cash@cashwu.com>
2017-08-29 15:11:28 +0800

    fix test case

--- Kata20170829_PlayingWithPassphrases/PlayingWithPassphrasesTests.cs
+++ Kata20170829_PlayingWithPassphrases/PlayingWithPassphrasesTests.cs
@@ -10,13 +10,13 @@ namespace Kata20170829_PlayingWithPassphrases
         [TestMethod]
         public void input_born_should_return_CpSo()
         {
-            PlayPassShouldBe("CpSo", "born", 1);
+            PlayPassShouldBe("oSpC", "born", 1);
         }
 
         [TestMethod]
         public void input_cash_should_return_CpSo()
         {
-            PlayPassShouldBe("DbTi", "cash", 1);
+            PlayPassShouldBe("iTbD", "cash", 1);
         }
 
         private static void PlayPassShouldBe(string expected, string source, int timer)
@@ -32,7 +32,7 @@ namespace Kata20170829_PlayingWithPassphrases
         public string playPass(string s, int n)
         {
             var result = s.ToLower().Select(c => (char)(c + 1)).Select((c, i) => i % 2== 0 ? char.ToUpper(c) : c);
-            return string.Concat(result);
+            return string.Concat(result.Reverse());
         }
     }
 }
